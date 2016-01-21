3f1de87e19d64347d632d87e1d0cb3d7532cbfaf
Salvatore Caschera <salvatore.caschera@lnf.infn.it>
2016-01-21 10:30:25 +0100

    classi scrapers 1.0

---
+++ models/technosoft/ClassesScraper1_0/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  ScrapersClasses
+//
+//  Created by MacBookProINFN on 19/01/16.
+//  Copyright © 2016 MacBookProINFN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    
+    
+    // host id del pc in the network deve essere specificato qui nel main:
+    // BYTE nHostID
+    
+    
+    
+    resp=TS_OpenChannel(CHANNEL_NAME, CHANNEL_TYPE, HOST_ID, BAUDRATE))
+    
+    std::cout << "Hello, World!\n";
+    
+    
+    
+    
+    
+    
+    return 0;
+}
