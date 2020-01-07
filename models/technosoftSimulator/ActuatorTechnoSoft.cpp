25645739599f09b941b0c9acb80cc640307c1ca7
amichelo <andrea.michelotti@lnf.infn.it>
2020-01-07 18:16:43 +0100

    poi

--- models/technosoftSimulator/ActuatorTechnoSoft.cpp
+++ models/technosoftSimulator/ActuatorTechnoSoft.cpp
@@ -101,8 +101,9 @@ int ActuatorTechnoSoft::init(void *initialization_string) {
 
 #ifdef CHAOS
   using namespace std;
-  if (initialization_string == NULL) {
+  if (initialization_string != NULL) {
     {
+      jsonConfiguration.setSerializedJsonData((const char*)initialization_string);
       GET_PARAMETER_TREE((&jsonConfiguration), driver_config) {
         GET_PARAMETER(driver_config, HostID, int32_t, 1);
         GET_PARAMETER(driver_config, serdev, string, 1);
@@ -127,7 +128,7 @@ int ActuatorTechnoSoft::init(void *initialization_string) {
   }
 #endif
 
-  std::string params;
+ /* std::string params;
   params.assign((const char *)initialization_string);
   boost::smatch match;
 
@@ -161,9 +162,12 @@ int ActuatorTechnoSoft::init(void *initialization_string) {
     }
     // initChannelAlreadyDone = true;
     return 0;
+  }*/
+  if(channel==NULL){
+    ERR("Cannot initialize with params %s", initialization_string?(const char*)initialization_string:"''");
+    return -4;
   }
-  ERR("Cannot possible init channel %s", params.c_str());
-  return -4;
+  return 0;
 }
 
 int ActuatorTechnoSoft::configAxis(void *initialization_string) {
